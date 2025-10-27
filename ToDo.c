65d32755ef9eb0c118d9f40af7fd88a1e1927d75
Vlad <vladvesanus.@gmail.com>
2025-10-27 09:35:22 +0100

    Change interface

--- ToDo.c
+++ ToDo.c
@@ -5,6 +5,7 @@
 char* read_from_user();
 char **updated_list(FILE *file, char *removed_duty);
 char **current_list(FILE *file);
+<<<<<<< HEAD
 
 int main()
 {
@@ -33,10 +34,38 @@ int main()
         }
         fclose(file);
         return 0;
+=======
+int add_duty();
+int delete_duty();
+
+int main()
+{
+    printf("\nYour duty: \n");
+    FILE *file = fopen("ToDo.txt", "r");
+    char **your_list = current_list(file);
+    fclose(file);
+    int i = 0;
+    while(your_list[i] != NULL)
+    {
+        printf("%s\n", your_list[i]);
+        free(your_list[i]);
+        i++;
+    }
+    free(your_list);
+    char c;
+    printf("\nWhat do you want to do(a - add, r - remove, e - exit) : ");
+    scanf(" %c", &c); //Choose the act
+    getchar() == '\n';//delete znak riadka
+    int result;
+    if (c == 'a')// add something
+    {
+        result = add_duty();
+>>>>>>> 0478828 (Change interface)
     }
     
     else if (c == 'r')// remove something
     {
+<<<<<<< HEAD
         char *removed_duty = NULL;
         while(1)
         {
@@ -91,6 +120,22 @@ int main()
         free(your_list);
     }
     
+=======
+        result = delete_duty();
+    }
+    else if (c == 'e')
+    {
+        return 0;
+    }
+    if (result == 0)
+    {
+        main();
+    }
+    else if (result == 1)
+    {
+        return 1;
+    }
+>>>>>>> 0478828 (Change interface)
 }
 
 char* read_from_user()
@@ -136,7 +181,11 @@ char **updated_list(FILE *file, char *removed_duty)
             str[len++] = (char)c;
             str[len] = '\0';
         } while(((c = fgetc(file)) !='\n') && (c != EOF));//read string char by char
+<<<<<<< HEAD
         if(strcmp(str, removed_duty) != 0)//write sting in new list
+=======
+        if(!strstr(str, removed_duty))//write sting in new list
+>>>>>>> 0478828 (Change interface)
         {
             updated_list = realloc(updated_list, sizeof(char*) * (count + 2));
             updated_list[count] = str;
@@ -178,4 +227,66 @@ char **current_list(FILE *file)
         current_list[++count] = NULL; 
     }
     return current_list;
+<<<<<<< HEAD
+=======
+}
+int add_duty()
+{
+    FILE *file = fopen("ToDo.txt", "a");
+    if (!file)
+    {
+        return 1;
+    }
+    printf("Duty: \n");
+    char *duty = read_from_user();
+    printf("When will it be finished: \n");
+    char *time = read_from_user();
+    fprintf(file,"%s %s\n", duty, time);
+    free(duty);
+    free(time);
+    fclose(file);
+    return 0;
+}
+int delete_duty()
+{
+    char *removed_duty = NULL;
+    while(1)
+    {
+        FILE *file = fopen("ToDo.txt", "r");
+        if(!file)
+        {
+            return 1;
+        }
+        removed_duty = read_from_user();
+        if(removed_duty == NULL)
+        {
+            fclose(file);
+            return 1;
+        }
+        if(strcmp(removed_duty, "stop") == 0)
+        {
+            fclose(file);
+            free(removed_duty);
+            break;
+        }
+        char **new_list = updated_list(file, removed_duty);
+        fclose(file);
+        if (new_list == NULL)
+        {
+            free(removed_duty);
+            return 1;
+        }
+        
+        FILE *new_file = fopen("ToDo.txt", "w");
+        int i = 0;
+        while(new_list[i] != NULL)//Write in new file and free memory
+        {
+            fprintf(new_file,"%s\n", new_list[i]);
+            free(new_list[i++]);
+        }
+        free(new_list);
+        fclose(new_file);
+        return 0;
+    }
+>>>>>>> 0478828 (Change interface)
 }
